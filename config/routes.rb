7388a86fa55c5521c4dd2cb9c58b3edd8e99595a
Jason Habing <jasonhabing@gmail.com>
2011-02-16 14:11:23 -0800

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ New::Application.routes.draw do
   get "pages/about"
 
   get "pages/home"
+  
+  root :to => "pages/home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +54,7 @@ New::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+
 
   # See how all your routes lay out with "rake routes"
 
